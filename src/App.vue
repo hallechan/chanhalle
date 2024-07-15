45dd1adffa431b8bffd9fb740c1a39024b919916
hailey <hallechanne@gmail.com>
2024-07-14 17:19:39 -0700

    everything!

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Navigation/>
+  <Home/>
+</template>
+
+<script>
+import Navigation from "./components/Navigation.vue";
+import Home from "./components/Home.vue";
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  components: {Navigation, Home}
+})
+
+
+</script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">
+<ol class="list-none list-inside flex justify-between">
+  <li><RouterLink to="/">home</RouterLink></li>
+  <li><RouterLink to="/">about</RouterLink></li>
+  <li><RouterLink to="/">design</RouterLink></li>
+  <li><RouterLink to="/">contact</RouterLink></li>
+</ol>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
