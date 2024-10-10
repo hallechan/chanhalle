9abb3950bb768784753f93d13a4356b959dd2558
halle <hallechanne@gmail.com>
2024-10-10 15:45:02 -0400

    Added pitta-patta to projects.

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -84,26 +84,66 @@
           </div>
         </li>
 
-        <!-- Project 3: Y2K Study-Buddy -->
+        <!-- Project 3: pitta-patta -->
+        <h4 class="font-inter font-semibold tracking-evenTighter text-5xl text-platinum mb-8">polyglot for perilled patois.</h4>
+        <li class="flex flex-row justify-between my-12 hover:cursor-pointer">
+          <a
+              class="relative w-2/5 h-2/5"
+              @mouseover="hoverIndex = 2"
+              @mouseleave="hoverIndex = -1"
+              href="https://devpost.com/software/pitta-patta"
+              target="_blank"
+          >
+            <img
+                src="../assets/images/projects/pittapatta/cover.png"
+                alt="Cover Image"
+                class="w-full h-full object-cover transition-opacity duration-300 ease-in-out glow-effect"
+                :class="{ 'opacity-0': hoverIndex === 2 }"
+            />
+            <img
+                src="../assets/images/projects/pittapatta/coverhover.png"
+                alt="Cover Hover Image"
+                class="absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-300 ease-in-out
+                hover:animate-jump hover:animate-duration-1000"
+                :class="{ 'opacity-100': hoverIndex === 2, 'opacity-0': hoverIndex !== 2 }"
+            />
+          </a>
+          <div class="w-1/2 flex flex-col justify-between">
+            <div>
+              <h4 class="font-inter font-semibold tracking-evenTighter text-4xl text-platinum max-w-lg break-words">
+                harnessing nlps thru <span class="text-dark-pink">pitta-patta</span> to fortify endangered dialects.
+              </h4>
+              <p class="font-inter text-lg text-grey tracking-tight leading-tight mt-4">
+                Hack the Valley 9 2024<BR/> <i class="opacity-75">Best Diversity Hack + Best Use of Streamlit</i><br/>
+                October 2024
+              </p>
+            </div>
+            <p class="font-inter text-lg text-platinum tracking-tight max-w-lg break-words ">
+              Playing with natural language processing to produce real-time translations for English-based dialects.
+            </p>
+          </div>
+        </li>
+
+        <!-- Project 4: Y2K Study-Buddy -->
         <h4 class="font-inter font-semibold tracking-evenTighter text-5xl text-platinum mb-8">y2k study-buddy.</h4>
         <li class="flex flex-row justify-between my-12">
           <div
               class="relative w-2/5 h-2/5"
-              @mouseover="hoverIndex = 2"
+              @mouseover="hoverIndex = 3"
               @mouseleave="hoverIndex = -1"
           >
             <img
                 src="../assets/images/projects/y2k/cover.png"
                 alt="Cover Image"
                 class="w-full h-full object-cover transition-opacity duration-300 ease-in-out glow-effect"
-                :class="{ 'opacity-0': hoverIndex === 2 }"
+                :class="{ 'opacity-0': hoverIndex === 3 }"
             />
             <img
                 src="../assets/images/projects/y2k/coverhover.png"
                 alt="Cover Hover Image"
                 class="absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-300 ease-in-out
                 hover:animate-jump hover:animate-duration-1000"
-                :class="{ 'opacity-100': hoverIndex === 2, 'opacity-0': hoverIndex !== 2 }"
+                :class="{ 'opacity-100': hoverIndex === 3, 'opacity-0': hoverIndex !== 3 }"
             />
           </div>
           <div class="w-1/2 flex flex-col justify-between">
