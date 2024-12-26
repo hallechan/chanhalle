49341ed31b59d21a0de99e90747846caad61a440
halle <hallechanne@gmail.com>
2024-12-26 02:32:24 -0500

    Added Suki Waterhouse tickets to design page.

--- src/components/Design.vue
+++ src/components/Design.vue
@@ -8,53 +8,61 @@
     </div>
     <div class="w-1/2 grid grid-cols-1 md:grid-cols-2 gap-12 animate-fade-up animate-delay-500">
       <div class="relative group">
+        <img src="../assets/images/gallery/tickets.png" alt="cupid" class="w-full aspect-square object-cover" />
+        <div class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+          <p class="text-platinum font-inter text-lg">suki waterhouse concert tickets</p>
+        </div>
+      </div>
+      <div class="relative group">
         <img src="../assets/images/gallery/cupid.png" alt="cupid" class="w-full aspect-square object-cover" />
         <div class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <p class="text-platinum font-inter text-lg">cupid</p>
+          <p class="text-platinum font-inter text-lg">stupid cupid</p>
         </div>
       </div>
       <div class="relative group">
         <img src="../assets/images/gallery/elixir.png" alt="elixir" class="w-full aspect-square object-cover" />
         <div class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <p class="text-platinum font-inter text-lg">elixir</p>
+          <p class="text-platinum font-inter text-lg">the elixir</p>
         </div>
       </div>
       <div class="relative group">
         <img src="../assets/images/gallery/golden.png" alt="golden" class="w-full aspect-square object-cover" />
         <div class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <p class="text-platinum font-inter text-lg">golden</p>
+          <p class="text-platinum font-inter text-lg">golden hour</p>
         </div>
       </div>
       <div class="relative group">
         <img src="../assets/images/gallery/kitchen.png" alt="kitchen" class="w-full aspect-square object-cover" />
         <div class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <p class="text-platinum font-inter text-lg">kitchen</p>
+          <p class="text-platinum font-inter text-lg">kitchen.</p>
         </div>
       </div>
       <div class="relative group">
         <img src="../assets/images/gallery/laundry.png" alt="laundry" class="w-full aspect-square object-cover" />
         <div class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <p class="text-platinum font-inter text-lg">laundry</p>
+          <p class="text-platinum font-inter text-lg">laundry.</p>
         </div>
       </div>
       <div class="relative group">
         <img src="../assets/images/gallery/lunar.png" alt="lunar" class="w-full aspect-square object-cover" />
         <div class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <p class="text-platinum font-inter text-lg">lunar</p>
+          <p class="text-platinum font-inter text-lg">lunar lounge</p>
         </div>
       </div>
       <div class="relative group">
         <img src="../assets/images/gallery/melody.png" alt="melody" class="w-full aspect-square object-cover" />
         <div class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <p class="text-platinum font-inter text-lg">melody</p>
+          <p class="text-platinum font-inter text-lg">mymelody charms</p>
         </div>
       </div>
       <div class="relative group">
         <img src="../assets/images/gallery/renee.png" alt="renee" class="w-full aspect-square object-cover" />
         <div class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <p class="text-platinum font-inter text-lg">renee</p>
+          <p class="text-platinum font-inter text-lg">renee rapp</p>
         </div>
       </div>
     </div>
   </div>
 </template>
+<script setup lang="ts">
+</script>
